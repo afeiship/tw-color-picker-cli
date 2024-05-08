7ed0d3244f4c00e4f5f8d102cea36601dbbdf600
afeiship <1290657123@qq.com>
2024-05-08 23:40:59 +0800

    feat: add process

--- bin/index.js
+++ bin/index.js
@@ -2,6 +2,8 @@
 
 import { Command, Option } from 'commander';
 import { createRequire } from 'module';
+import nx from '@jswork/next';
+import colors from './colors.js';
 
 const __dirname = new URL('../', import.meta.url).pathname;
 const require = createRequire(__dirname);
@@ -11,10 +13,63 @@ const program = new Command();
 program.version(pkg.version);
 program
   .addOption(new Option('-v, --verbose', 'show verbose log'))
-  .addOption(new Option('-f, --force', 'force to create'))
-  .addOption(new Option('-c, --city <string>', 'weather of city').choices(['wuhan', 'shanghai']))
+  .addOption(new Option('-i, --input <string>', 'input color'))
   .parse(process.argv);
 
+function deltaE(rgbA, rgbB) {
+  let labA = rgb2lab(rgbA);
+  let labB = rgb2lab(rgbB);
+  let deltaL = labA[0] - labB[0];
+  let deltaA = labA[1] - labB[1];
+  let deltaB = labA[2] - labB[2];
+  let c1 = Math.sqrt(labA[1] * labA[1] + labA[2] * labA[2]);
+  let c2 = Math.sqrt(labB[1] * labB[1] + labB[2] * labB[2]);
+  let deltaC = c1 - c2;
+  let deltaH = deltaA * deltaA + deltaB * deltaB - deltaC * deltaC;
+  deltaH = deltaH < 0 ? 0 : Math.sqrt(deltaH);
+  let sc = 1.0 + 0.045 * c1;
+  let sh = 1.0 + 0.015 * c1;
+  let deltaLKlsl = deltaL / 1.0;
+  let deltaCkcsc = deltaC / sc;
+  let deltaHkhsh = deltaH / sh;
+  let i = deltaLKlsl * deltaLKlsl + deltaCkcsc * deltaCkcsc + deltaHkhsh * deltaHkhsh;
+  return i < 0 ? 0 : Math.sqrt(i);
+}
+
+function rgb2lab(rgb) {
+  let r = rgb[0] / 255,
+    g = rgb[1] / 255,
+    b = rgb[2] / 255,
+    x,
+    y,
+    z;
+  r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+  g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+  b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+  x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+  y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
+  z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+  x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
+  y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
+  z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
+  return [116 * y - 16, 500 * (x - y), 200 * (y - z)];
+}
+
+// #fff -> hex(255, 255, 255)
+function color2hex(color) {
+  let hex = color.replace('#', '');
+  if (hex.length === 3) {
+    hex = hex
+      .split('')
+      .map((x) => x + x)
+      .join('');
+  }
+  return [
+    parseInt(hex.substr(0, 2), 16),
+    parseInt(hex.substr(2, 2), 16),
+    parseInt(hex.substr(4, 2), 16),
+  ];
+}
 /**
  * @help: tcpc -h
  * @description: tcpc -f
@@ -32,7 +87,33 @@ class CliApp {
   }
 
   run() {
-    this.log('run cli: ', __dirname, this.args, this.opts, pkg.version);
+    console.log('colors: ', this.opts);
+    const { input } = this.opts;
+    const results = [];
+    for (let key in colors) {
+      const value = colors[key];
+      const isColor = typeof value === 'string';
+      if (isColor) {
+        const v1 = color2hex(input);
+        const v2 = color2hex(value);
+        results.push({ key, value, deltaE: deltaE(v1, v2) });
+        console.log('input/value/delta-e: ', input, value, deltaE(v1, v2));
+      } else {
+        nx.forIn(value, (k, v) => {
+          const subV1 = color2hex(input);
+          const subV2 = color2hex(v);
+          results.push({ key, k, v, deltaE: deltaE(subV1, subV2) });
+        });
+      }
+    }
+    const nums = results.map((item) => item.deltaE || 100);
+    const getMinItem = () => {
+      const min = Math.min(...nums);
+      return results.find((item) => item.deltaE === min);
+    };
+    const minItem = getMinItem();
+    console.log('minItem: ', minItem);
+    // this.log('run cli: ', __dirname, this.args, this.opts, pkg.version);
   }
 }
 
